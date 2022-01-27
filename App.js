0a3957f8fd92ef26d3323a3d150104a5af8aabd0
shipsy <semal.sherathia@shipsy.io>
2022-01-27 17:31:19 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res) {
+  res.send("Hello World");
+});
+
+app.get('/series', function(req, res) {
+  const response = [
+    'Mouse', 'Demon Slayer', 'The Office'
+  ]
+  res.send(response)
+})
+
+app.listen(4003);
